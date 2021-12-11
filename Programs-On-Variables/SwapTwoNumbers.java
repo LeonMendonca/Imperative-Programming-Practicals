619e404e082b921627a4135696c376588b48a13b
schmeekygeek <ss6087982@gmail.com>
2021-12-11 15:30:01 +0530

    basic optimizations

--- Programs-On-Variables/SwapTwoNumbers.java
+++ Programs-On-Variables/SwapTwoNumbers.java
@@ -1,9 +1,12 @@
 //Write a program to swap two numbers without third variable
+import java.util.Scanner;
 
 class SwapTwoNumbers{
     public static void main(String[] args){
-        int num1 = 7;       // Sample variable 1
-        int num2 = 3;       // Sample variable 2
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter 1st and 2nd number: ");
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
         System.out.println(String.format("Numbers before swap: %d %d",num1,num2));
         //Begin swap
         num1 = num1+num2;
@@ -11,5 +14,6 @@ class SwapTwoNumbers{
         num1 = num1-num2;
         //End swap
         System.out.println(String.format("Numbers after swap: %d %d",num1,num2));
+        scan.close();
     }
 }
